188cbe1b46767c27f414e03697a865cab85b8374
ikerrios9 <1925184@alu.murciaeduca.es>
2026-01-14 17:56:24 +0100

    proyecto acabado

--- SEGUNDO TRIMESTRE/nivel1_react/my-react-app/src/App.jsx
+++ SEGUNDO TRIMESTRE/nivel1_react/my-react-app/src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-    return <h1>Nivel 1 de React desbloqueado</h1>
+export default function App() {
+    return <ParentComponent />
 }
 
-export default App
+function ParentComponent() {
+    return (
+    <div>
+        <UserComponent />
+        <ProfileComponent />
+        <FeedComponent />
+    </div>
+    )
+}
+
+function UserComponent() {
+    return <h2>User component</h2>
+}
+
+function ProfileComponent() {
+    return <h2>Profile component</h2>
+}
+
+function FeedComponent() {
+    return <h2>Feed component</h2>
+}
+
