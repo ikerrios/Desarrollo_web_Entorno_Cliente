cdb5cd7874be328353153cc4f06f99939378469c
ikerrios9 <1925184@alu.murciaeduca.es>
2026-01-16 19:38:07 +0100

    nivel 4 react

--- SEGUNDO TRIMESTRE/Tema 5/nivel_react/my-react-app/src/App.jsx
+++ SEGUNDO TRIMESTRE/Tema 5/nivel_react/my-react-app/src/App.jsx
@@ -1,27 +1,28 @@
-function App() {
-    const estilos = 'estiloCSS'
+import './index.css'
+import { useState } from 'react'
 
-    const users = [
-        { id: 1, name: 'Jose', role: 'Web Developer' },
-        { id: 2, name: 'Estefanía', role: 'Web Designer' },
-        { id: 3, name: 'Rubén', role: 'Team Leader' },
-        { id: 4, name: 'Iker', role: 'El puto jefe' },
-    ]
-    
-    return (
-        <>
-        <p>Lista de usuarios activos:</p>
-        <ul>
-        {users.map(function (user) {
-            return (
-            <li key={user.id} className="estilosCSS">
-                {user.name} — {user.role}
-            </li>
-            )
-        })}
-        </ul>
-        </>
-    )
+function ChildComponent(props) {
+  return (
+    <>
+      <h1>Hello {props.name}</h1>
+      <button onClick={() => props.setName("Cipriano")}>Cambiar a Cipriano</button>
+      <br/>
+      <button onClick={() => props.setName("Klara")}>Cambiar a Klara</button>
+    </>
+  )
+}
+
+function ParentComponent() {
+  const [name, setName] = useState("Iker")
+  return <ChildComponent name={name} setName = {setName}/>
+}
+
+function App() {
+  return (
+    <div>
+      <ParentComponent />
+    </div>
+  )
 }
 
 export default App
